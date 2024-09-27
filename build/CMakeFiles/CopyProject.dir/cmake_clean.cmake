ff6471e33a649047622a276d8eac1682e5c7e883
Kanngetu <18811693087@163.com>
2024-09-27 21:14:20 +0800

    Add files via upload

---
+++ build/CMakeFiles/CopyProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CopyProject.dir/HW.cpp.obj"
+  "CMakeFiles/CopyProject.dir/HW.cpp.obj.d"
+  "CopyProject.exe"
+  "CopyProject.exe.manifest"
+  "CopyProject.pdb"
+  "libCopyProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CopyProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/HW.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW.dir/HW.cpp.obj"
+  "CMakeFiles/HW.dir/HW.cpp.obj.d"
+  "HW.exe"
+  "HW.exe.manifest"
+  "HW.pdb"
+  "libHW.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
